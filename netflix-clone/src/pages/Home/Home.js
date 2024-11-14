e5fb49d96cc339e37f53c13d4e204dcb9af30fad
tumimg <getachewtumim@gmail.com>
2024-11-14 01:44:23 -0800

    axios,banner and request

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-// import Banner from '../../components/Banner/Banner'
+import Banner from '../../components/Banner/Banner'
 // import RowList from '../../components/Rows/Rowlist/Rowlist'
 
 const Home = () => {
     return (
         <>
             <Header />
-            {/* <Banner />
-            <RowList /> */}
-            <Footer />
-        </>
-    )
-}
+            <Banner />
+             <Footer />
+         </>
+     )
+ }
 
 export default Home
