2d5a89c71174c288fa8e59fb86799bf638884cba
tumimg <getachewtumim@gmail.com>
2024-11-16 21:25:21 -0800

    Row

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react'
 import axios from "../../utils/axios";
 import requests from '../../utils/requests';
--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner'
-// import RowList from '../../components/Rows/Rowlist/Rowlist'
+import RowList from '../../components/Rows/RowList/RowList'
 
 const Home = () => {
     return (
         <>
             <Header />
             <Banner />
+            <RowList/>
              <Footer />
          </>
      )
